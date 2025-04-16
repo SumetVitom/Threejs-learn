259efddc1d275a0b9fbf18c4d5a31ca1b95d09fb
SumetVitom <sumet.supprasert@vitom-tech.com>
2025-04-16 18:00:58 +0900

    additional updates

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
